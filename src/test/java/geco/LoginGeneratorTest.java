ae6bc9b0d84a1035c417d7a7312c52adce1494bb
xRevenger <chukewin@gmail.com>
2020-11-17 09:24:37 +0100

    #2

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,16 @@
+package geco;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginGeneratorTest {
+
+    private LoginService loginService = new LoginService(new String[] {"JROL", "BPER", "CGUR", "JDU", "JRAL", "JRAL1"});
+    private LoginGenerator loginGenerator = new LoginGenerator(loginService);
+
+    @Test
+    public void generateLoginForNomAndPrenom() {
+        assertEquals("Le login créé ne correspond pas.", "PDUR", loginGenerator.generateLoginForNomAndPrenom("DURAND","PAUL"));
+    }
+}
