3a73834faa9f3c3af458b2599d93f93142726487
xRevenger <chukewin@gmail.com>
2020-11-17 09:27:14 +0100

    fix #3

--- src/test/java/geco/LoginGeneratorTest.java
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -13,4 +13,9 @@ public class LoginGeneratorTest {
     public void generateLoginForNomAndPrenom() {
         assertEquals("Le login créé ne correspond pas.", "PDUR", loginGenerator.generateLoginForNomAndPrenom("DURAND","PAUL"));
     }
+
+    @Test
+    public void testJRAL2() {
+        assertEquals("Le login créé ne correspond pas.", "JRAL2", loginGenerator.generateLoginForNomAndPrenom("RALLING","JOHN"));
+    }
 }
